041d5382fd7252bdae3a1a85f1ea9d721d5e05a4
Eurus <10878318+e_urus@user.noreply.gitee.com>
2024-03-17 19:30:16 +0800

    反向推理，两种方案细分。这个版本记录了失败的直接细分，原因是无法处理多个键对应一个值的问题。

--- production_system.py
+++ production_system.py
@@ -2,7 +2,7 @@ def judge(knowledge,dic,dic_flag):
     while True:
         flag=0
         for k in dic:
-            if dic_flag[k]==0 and dic[k] not in knowledge and all(i in knowledge for i in k) :
+            if dic_flag[k]==0 and dic[k] not in knowledge and all(i in knowledge for i in k) : #未被检测+不在已有之内+被包含
                 knowledge.append(dic[k])
                 print(k,"-->",dic[k],knowledge)
                 dic_flag[k]=1
@@ -10,20 +10,54 @@ def judge(knowledge,dic,dic_flag):
         if flag==0:
             return knowledge
 
-def back(knowledge,dic,dic_flag,result):
+def back_recursion(dic,known,data_base):# 递归对元素进行分解 分解后判断
+    for j in data_base:
+        for i in dic:
+
+            if dic[i]==j:
+                data_base_copy=data_base.copy()
+                for k in i:
+                    data_base_copy.append(k)
+                data_base_copy.remove(j)
+                print("字典从值寻到键",j,"-->",i)
+                #print(data_base_copy)
+                print("尝试匹配",data_base_copy,known)
+                if all( m in known for m in data_base_copy):
+                    print("匹配成功")
+                    return 1
+                else:
+                    result=back_recursion(dic, known, data_base_copy)
+                    if result:
+                        return result
+
+"""
+def back(knowledge,result,end_body):
     print("反向推理")
-    for i in result:
-        dic[i]
+    tmp=[]
+    for i in end_body:
+        print("提出假设",i)
+        tmp=[i]
 
-def find_key(input_dict, value):
-    output()
-    for k, v in input_dict.items():
-        if v == value:
-            k
-    return None
-
+        div_flag=1
+        while div_flag == 1:
+            div_flag=0
+            for k in list(tmp):
+                print(tmp)
+                if not all(j in knowledge for j in tmp): # 如果已知未能包含全部已有的假设项
+                    print("假设不成立，尝试获取假设的细分")
+                    if k in result: # 进一步细分假设项
+                        for l in result[k]:
+                            tmp.append(l) #增加k的细分，删除k
+                        tmp.remove(k)
+                        div_flag=1
+                        #k=0
+                    else:print("%d无法细分",k)
+                else:
+                    print("假设成立",i)
+"""
 
 if __name__ == '__main__':
+    #数据初始化
     a = ["1毛发", "2奶", "3羽毛", "4会飞", "5吃肉", "6犬齿", "7有爪", "8眼盯前方", "9有蹄",
          "10反刍动物", "11哺乳动物", "12鸟", "13善飞", "14信天翁", "15食肉动物", "16黄褐色", "17暗斑点",
          "18黑色条纹", "19长腿", "20长脖子", "21有蹄类动物", "22虎", "23金钱豹", "24长颈鹿", "25斑马",
@@ -31,23 +65,42 @@ if __name__ == '__main__':
     r = [[1, 11], [2, 11], [3, 12], [4, 31, 12], [5, 15], [6, 7, 8, 15], [11, 9, 21], [11, 10, 21],
          [11, 15, 16, 17, 23], [11, 15, 16, 18, 22], [21, 20, 19, 17, 24],
          [21, 18, 25], [12, 20, 26, 27, 24, 29], [12, 28, 16, 27, 30], [12, 13, 14]]
-
-
-
     r_dict = {}
     r_flag={}
     for item in r:
-
         key = tuple(item[:-1])  # 将除最后一位的部分作为键
         value = item[-1]  # 将最后一位作为值
         r_dict[key] = value
         r_flag[key] = 0
-    print(r_dict,r_flag)
+    #print(r_dict,r_flag)
+    #print(len(r), len(r_flag))
 
-    print(len(r), len(r_flag))
+    #反向推理数据初始化
+    end_body=[22,23,24,25,29,30,14]
+    back_dict={}
+    back_flag={}
+    for i in r:
+        key = i[-1]
+        value= tuple(i[:-1])
+        back_dict[key]=value
+        back_flag[key]=0
+    print(back_dict,back_flag)
+
+
+
+    #正向推理
     k=[17,20,19,2,9]
-    knowledge=judge(k,r_dict,r_flag)
-    print(knowledge)
+    #knowledge=judge(k,r_dict,r_flag)
+    #print(knowledge)
 
-
+    k = [17, 20, 19, 2, 9]
+    #back(k,back_dict,end_body)
+    #data_base=[22]
+    for q in end_body:
+        print("")
+        print("假设是",q)
+        result=back_recursion(r_dict,k,[q])
+        if result:
+            print("结果是",q)
+            break
 
